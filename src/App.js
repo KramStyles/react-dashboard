cdf1dc44ff41ba77de2f015441b52d3e77aaba3b
kramstyles <mark.eke@decagon.dev>
2022-05-31 00:56:44 +0100

    added extra data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import 'App.css'
+import './App.css'
 
 const App = () => {
     return (
         <>
-            App
+            <h1 className={"underline text-3xl"}>App</h1>
         </>
     )
 }
