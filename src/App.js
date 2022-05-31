2c4f6872cf63a1deb5829d0e0b7d4a4a179b8729
kramstyles <mark.eke@decagon.dev>
2022-05-31 01:14:11 +0100

    added bottom settings

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {FiSettings} from "react-icons/fi";
+import {TooltipComponent} from "@syncfusion/ej2-react-popups";
+import {useEffect} from "react";
+
 import './App.css'
 
 const App = () => {
     return (
         <>
-            <h1 className={"underline text-3xl"}>App</h1>
+            <Router>
+                <div className="flex relative dark:bg-main-dark-bg">
+                    <div className="fixed right-4 bottom-4">
+                        <TooltipComponent content={'settings'} position={'Top'}>
+                            <button type={'button'} className={'text-3xl p-3 text-white'} style={{background: 'dodgerblue', borderRadius: '50%'}}><FiSettings/></button>
+                        </TooltipComponent>
+                    </div>
+                </div>
+            </Router>
         </>
     )
 }
